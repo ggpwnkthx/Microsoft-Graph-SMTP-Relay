c7764b856932083aa38a7a5345f2e0fd81543a3b
Isaac Jessup <ibjessup@gmail.com>
2024-11-22 13:33:18 +0000

    non-inline attachment test

--- test/client.py
+++ test/client.py
@@ -1,6 +1,7 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
 import logging, os, smtplib
 
 # Set the logging level to DEBUG for verbose output
@@ -22,15 +23,11 @@ smtp_port = int(os.environ.get("SMTP_RELAY_PORT", 25))
 smtp_user = "your-email@example.com"  # This should be replaced with a real username
 smtp_password = "your-password"  # This should be replaced with a real password
 
-# Sender and recipient email addresses
-from_email = "me@company.com"
-to_email = "them@gmail.com"
-
 # Create a MIMEMultipart message object for an email with mixed content (HTML and image)
 msg = MIMEMultipart("related")
 msg["Subject"] = "Email with HTML and Embedded Image"
-msg["From"] = from_email
-msg["To"] = to_email
+msg["From"] = os.environ.get("TEST_FROM_ADDRESS", "me@company.com")
+msg["To"] = os.environ.get("TEST_TO_ADDRESS", "me@company.com")
 msg.preamble = "This is a multi-part message in MIME format."
 
 # HTML version of the email body
@@ -56,13 +53,19 @@ with open("test/image.jpg", "rb") as img:
     mime_image.add_header("Content-Disposition", "inline", filename="image.jpg")
     msg.attach(mime_image)
 
+# Open the PDF file and attach it
+with open("test/doc.pdf", "rb") as pdf_file:
+    mime_pdf = MIMEApplication(pdf_file.read(), _subtype="pdf")
+    mime_pdf.add_header("Content-Disposition", "attachment", filename="doc.pdf")
+    msg.attach(mime_pdf)
+
 # Log the attempt to connect to the SMTP server
 logging.info(f"Attempting to connect to SMTP service at {smtp_server}:{smtp_port}")
 
 # Connect to the SMTP server, authenticate, and send the email
 with smtplib.SMTP(smtp_server, smtp_port) as server:
     server.login(smtp_user, smtp_password)
-    server.sendmail(from_email, to_email, msg.as_string())
+    server.sendmail(msg["From"], msg["To"], msg.as_string())
 
 # Confirmation message after sending the email
 print("Email sent!")
